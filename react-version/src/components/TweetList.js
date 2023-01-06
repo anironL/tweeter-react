993fd0a78b79e9eb818b6dbdc54acf3a906eae57
anironL <viasolaris@gmail.com>
2023-01-06 17:07:04 -0500

    convert hardcoded values to variables

--- react-version/src/components/TweetList.js
+++ react-version/src/components/TweetList.js
@@ -1,51 +1,59 @@
 import React from 'react';
 
 function TweetList(){
+  const tweet = {
+    avatar: "https://i.imgur.com/73hZDYK.png",
+    firstName: "Isaac",
+    lastName: "Newton",
+    handle: "@SirIsaac",
+    body: "If I have seen further it is by standing on the shoulders of giants",
+    age: "10 days ago"
+  }
+  
   return (
-    
-    <section className="tweets">
-  <article className="tweet">
-    <header className="tweet--header">
-      <img className="tweet--avatar" src="https://i.imgur.com/73hZDYK.png" />
-      <h2 className="tweet--name">Newton</h2>
-      <small className="tweet--handle">@SirIsaac</small>
-    </header>
+  <section className="tweets">
+    <article className="tweet">
+      <header className="tweet--header">
+        <img className="tweet--avatar" src={tweet.avatar} />
+        <h2 className="tweet--name">{tweet.lastName}</h2>
+        <small className="tweet--handle">{tweet.handle}</small>
+      </header>
 
-    <div className="tweet--body">
-      <p>If I have seen further it is by standing on the shoulders of giants</p>
-    </div>
+      <div className="tweet--body">
+        <p>{tweet.body}</p>
+      </div>
 
-    <footer className="tweet--footer">
-      <small className="footer--age">10 days ago<small>
+      <footer className="tweet--footer">
+        <small className="footer--age">{tweet.age}<small>
           <span className="footer--actions">
             <a href="#"><i className="fa fa-flag"></i></a>
             <a href="#"><i className="fa fa-retweet"></i></a>
             <a href="#"><i className="fa fa-heart"></i></a>'
           </span>
         </small></small></footer>
-  </article>
-  <article className="tweet">
-    <header className="tweet--header">
-      <img className="tweet--avatar" src="https://i.imgur.com/nlhLi3I.png" />
-      <h2 className="tweet--name">Descartes</h2>
-      <small className="tweet--handle">@rd</small>
-    </header>
+    </article>
+    <article className="tweet">
+      <header className="tweet--header">
+        <img className="tweet--avatar" src={tweet.avatar} />
+        <h2 className="tweet--name">{tweet.lastName}</h2>
+        <small className="tweet--handle">{tweet.handle}</small>
+      </header>
 
-    <div className="tweet--body">
-      <p>Je pense , donc je suis</p>
-    </div>
+      <div className="tweet--body">
+        <p>{tweet.body}</p>
+      </div>
 
-    <footer className="tweet--footer">
-      <small className="footer--age">10 days ago<small>
+      <footer className="tweet--footer">
+        <small className="footer--age">{tweet.age}<small>
           <span className="footer--actions">
             <a href="#"><i className="fa fa-flag"></i></a>
             <a href="#"><i className="fa fa-retweet"></i></a>
             <a href="#"><i className="fa fa-heart"></i></a>'
           </span>
         </small></small></footer>
-  </article>
+    </article>
 
-</section>
+  </section>
   );
 }
 
