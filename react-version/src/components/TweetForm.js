325551b65e837dd829a84925119ac72a7145c3e7
anironL <viasolaris@gmail.com>
2023-01-06 17:25:40 -0500

    Stop submit button behavior

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function TweetForm(){
+  function stopSubmit(e) {
+    e.preventDefault();
+    console.log("no submit")
+  }
   const placeholderText = "What are you humming about?"
   
   return (
     <section className="newtweet">
-    <form method="post" action="/tweets" className="newtweet__form">
+    <form onSubmit={stopSubmit} method="post" action="/tweets" className="newtweet__form">
       <textarea className="form__textarea" name="text" placeholder={placeholderText}></textarea>
       <input type="submit" value="Tweet" className="form__input" />
       <span className="form__counter">140</span>
